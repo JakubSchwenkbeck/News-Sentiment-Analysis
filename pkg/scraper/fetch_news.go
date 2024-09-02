31445531d0ee86c0ee0c5c85093d907db21e67f9
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-09-02 15:50:03 +0200

    Colly Scraper with UserAgent mimicking Webbrowser

--- pkg/scraper/fetch_news.go
+++ pkg/scraper/fetch_news.go
@@ -9,11 +9,22 @@ import (
 
 // Article represents the structure of the news article data.
 type Article struct {
-	Title   string
-	Content string
+	Title   string // Title of the news article
+	Content string // Content of the news article
 }
 
 // fetchBBCNews fetches news articles from the BBC News website.
+//
+// It creates a new Colly collector, sets the User-Agent to mimic a web browser,
+// and defines callbacks for HTML elements to extract article titles and content.
+// It visits the specified URL and returns a slice of Article structs and any error encountered.
+//
+// Parameters:
+//   - url (string): The URL of the BBC News website to scrape.
+//
+// Returns:
+//   - ([]Article, error): A slice of Article structs containing the title and content of news articles,
+//     and an error if any occurs during the process.
 func fetchBBCNews(url string) ([]Article, error) {
 	var articles []Article
 
@@ -23,7 +34,7 @@ func fetchBBCNews(url string) ([]Article, error) {
 	// Set the User-Agent to mimic a web browser
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
-	// Set up a callback for when a HTML element is found
+	// Set up a callback for when an HTML element with class 'gs-c-promo-heading' is found
 	c.OnHTML(".gs-c-promo-heading", func(e *colly.HTMLElement) {
 		title := e.Text
 		link := e.Attr("href")
@@ -34,7 +45,7 @@ func fetchBBCNews(url string) ([]Article, error) {
 		}
 	})
 
-	// Set up a callback for when a HTML element is found
+	// Set up a callback for when an HTML element with class 'ssrcss-uf6wea-RichTextComponentWrapper' is found
 	c.OnHTML(".ssrcss-uf6wea-RichTextComponentWrapper", func(e *colly.HTMLElement) {
 		content := e.Text
 		if len(articles) > 0 {
@@ -56,6 +67,11 @@ func fetchBBCNews(url string) ([]Article, error) {
 	return articles, nil
 }
 
+// main is the entry point of the application. It fetches news articles from the BBC News website
+// and prints the title and content of each article.
+//
+// It calls the fetchBBCNews function with the BBC News URL, handles any errors that occur,
+// and outputs the results to the console.
 func main() {
 	url := "https://www.bbc.com/news"
 	articles, err := fetchBBCNews(url)
